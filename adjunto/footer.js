4d2f8ee8cfc1138e53b95f102cf0fceba463866d
Lautaro-Bramajo <128433273+Lautaro-Bramajo@users.noreply.github.com>
2023-04-04 15:37:25 -0300

    Add files via upload

---
+++ adjunto/footer.js
@@ -0,0 +1,25 @@
+
+console.dir(document)
+
+const footerHTML = document.getElementById("footer")
+
+
+footerHTML.innerHTML += `
+            
+    <div>
+        <nav>
+            <div>
+                <h3>Desarrollado por:</h3>
+                <p>Bramajo Lautaro</p>
+                <p></p>
+            </div>
+            <div>
+                <h3>Contacto</h3>
+                <a href="https://chat.whatsapp.com/JhmA05le4K2A4BpvMH7Beu"><i class="bi bi-whatsapp"></i> Whatsapp</a>
+            </div>
+        </nav>
+        <br>
+        <p>®Todos los derechos reservados</p>
+    </div>
+       
+`
