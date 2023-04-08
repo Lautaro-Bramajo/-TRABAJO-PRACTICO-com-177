9dcdb85a9bb3747bfc19259eae50ab27a564cfbd
Lautaro-Bramajo <128433273+Lautaro-Bramajo@users.noreply.github.com>
2023-04-08 20:18:45 -0300

    Update footer.js

--- adjunto/footer.js
+++ adjunto/footer.js
@@ -15,7 +15,9 @@ footerHTML.innerHTML += `
             </div>
             <div>
                 <h3>Contacto</h3>
-                <a href="https://chat.whatsapp.com/JhmA05le4K2A4BpvMH7Beu"><i class="bi bi-whatsapp"></i> Whatsapp</a>
+                <a href="https://chat.whatsapp.com/JhmA05le4K2A4BpvMH7Beu" class="what"><i class="bi bi-whatsapp"></i> Whatsapp</a>
+                <a href="https://www.youtube.com/@argentinaprograma4.0" class="youtube"><i class="bi bi-youtube"></i> Youtube</a>
+                <a href="https://discord.gg/QBCFkzTcvJ" class="discord"><i class="bi bi-discord"></i> Discord</a>
             </div>
         </nav>
         <br>
